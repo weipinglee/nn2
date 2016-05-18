97af9b28b3c7e339cd2bae02fdc658ee1b824b1f
weiping <weiping.lee@163.com>
2016-05-18 17:09:31 +0800

    kj

--- libs/Library/configs.php
+++ libs/Library/configs.php
@@ -10,9 +10,9 @@ namespace Library;
 return array(
     'http'=>'http',
     'host'=>array(//各系统域名路�?
-        'deal'=>'http://124.166.246.120:8000/nn2/deal/public',
-        'user'=>'http://124.166.246.120:8000/nn2/user/public',
-        'admin'=>'http://124.166.246.120:8000/nn2/admin/public'
+        'deal'=>'http://deal.test.com',
+        'user'=>'http://user.test.com',
+        'admin'=>'http://admin.test.com'
     ),
     'rootDir'=>array(//项目根目录绝对路�?
         'user'=>'E:\wamp\www\nn2\user\public',
