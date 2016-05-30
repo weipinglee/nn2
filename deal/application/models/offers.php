b02412a281af5abb6b22c7f0c4c0d53bd52af381
weipinglee <weiping.lee@163.com>
2016-05-30 13:54:03 +0800

    heb

--- deal/application/models/offers.php
+++ deal/application/models/offers.php
@@ -2,19 +2,76 @@
 /**
  * User: maoyong
  * Date: 2016/5/17 0017
- * Time: ÏÂÎç 5:05
+ * Time: ???? 5:05
  */
 
 use \Library\Query;
+use \Library\M;
 class offersModel extends \nainai\offer\product{
 
+
+    private $offer ;
+    public function __construct(){
+        $this->offer = new M('product_offer');
+    }
+
+    public function getList($page,$where =''){
+        $query = new Query('product_offer as o');
+        $query->join = "left join products as p on o.product_id = p.id ";
+        $query->fields = "o.*,p.cate_id,p.name,p.quantity,p.freeze,p.sell,p.unit,o.price,o.accept_area,p.produce_area,p.id as product_id";
+        if($where) $query->where = $where;
+        $query->page = $page;
+        $query->pagesize = 5;
+        $query->order = "apply_time desc";
+        $data = $query->find();
+        foreach ($data as $key => &$value) {
+            $value['mode_txt'] = $this->offerMode($value['mode']);
+            $value['img'] = empty($value['img']) ? 'no_picture.jpg' : $value['img'];//è·å–ç¼©ç•¥å›¾
+            $value['left'] = number_format(floatval($value['quantity']) - floatval($value['freeze']) - floatval($value['sell']),2);
+        }
+        $pageBar =  $query->getPageBar();
+        return array('data'=>$data,'bar'=>$pageBar);
+    }
+
+    //è·å–æŠ¥ç›˜ç±»å‹
+    public function offerMode($type){
+        return $this->getMode($type);
+    }
+
     /**
-     * »ñÈ¡²úÆ·¶ÔÓ¦·ÖÀàÏÂµÄµÄ±¨ÅÌĞÅÏ¢ÁĞ±í
-     * @param  [Int] $cateId [·ÖÀàid]
+     * è·å–æŠ¥ç›˜è¯¦æƒ…
+     */
+    public function offerDetail($id){
+        $query = new Query('product_offer as o');
+        $query->join = "left join products as p on o.product_id = p.id left join product_photos as pp on p.id = pp.products_id";
+        $query->fields = "o.*,p.cate_id,p.name,pp.img,p.quantity,p.freeze,p.sell,p.unit";
+        $query->where = 'o.id = :id';
+        $query->bind = array('id'=>$id);
+        $res = $query->getObj();
+
+        if(!empty($res)){
+            $res['mode_text'] = $this->offerMode($res['mode']);
+            $res['img'] = empty($res['img']) ? 'no_picture.jpg' : \Library\thumb::get($res['img'],100,100);//è·å–ç¼©ç•¥å›¾
+            $res['left'] = number_format(floatval($res['quantity']) - floatval($res['freeze']) - floatval($res['sell']),2);
+            if($res['divide']==self::UNDIVIDE)
+                $res['minimum'] = $res['quantity'];
+        }
+
+
+        return $res ? $res : array();
+    }
+
+    //è·å–æŠ¥ç›˜ç±»å‹
+    public function offerType($id){
+        return intval($this->offer->where(array('id'=>$id))->getField('mode'));
+    }
+    /**
+     * é¦–é¡µè·å–åˆ†ç±»æŠ¥ç›˜
+     * @param  [Int] $cateId å¤§ç±»id
      * @return [Array]
      */
     /**
-     * ×Ô¶¨ÒåµÄmysqlº¯Êı
+     * mysqlå‡½æ•°
      * getChildLists(rootId)
      *  BEGIN
         DECLARE sTemp VARCHAR(1000);
@@ -41,5 +98,8 @@ class offersModel extends \nainai\offer\product{
 
 
     }
+
+
+
 
 }
