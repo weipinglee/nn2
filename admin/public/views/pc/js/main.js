783268e4409e4272351a5e755736d7c475d1c281
lpf8090y <848062514@qq.com>
2016-08-19 10:12:35 +0800

    全选BUG434

--- admin/public/views/pc/js/main.js
+++ admin/public/views/pc/js/main.js
@@ -436,3 +436,26 @@ jQuery.extend( jQuery.easing,
 		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
 	}
 });
+
+
+$(function(){ //调用全选插件
+  $.fn.check({checkall_name:"checkall",checkbox_name:"check"})}),
+
+//全选插件
+(function($) {
+    $.fn.check    = function(options) {
+    var defaults  = {
+        checkall_name: "checkall_name", //全选框name
+        checkbox_name: "checkbox_name" //单选框name
+    },
+    ops           = $.extend(defaults, options);
+    e             = $("input[name='" + ops.checkall_name + "']"), //全选
+    f             = $("input[name='" + ops.checkbox_name + "']"), //单选
+    g             = f.length; //单选数量
+    f.click(function() {
+    $("input[name ='" + ops.checkbox_name + "']:checked").length == $("input[name='" + ops.checkbox_name + "']").length ? e.attr("checked", !0) : e.attr("checked", !1);
+    }), e.click(function() {
+    for (i = 0; g > i; i++) f[i].checked = this.checked;
+    });
+    };
+})(jQuery);
