9797c86c8bb5276a871d7de72787b568300d9f87
zyn1 <739619279@qq.com>
2016-08-25 13:09:01 +0800

    配置文件

--- libs/Library/configs.php
+++ libs/Library/configs.php
@@ -8,14 +8,14 @@
 namespace Library;
 return array(
     'host'=>array(//各子系统域名列表，最后不加'/'
-        'deal'=>'http://localhost/nn2/deal/public',
-        'user'=>'http://localhost/nn2/user/public',
-        'admin'=>'http://localhost/nn2/admin/public',
+        'deal'=>'http://124.166.246.120:8000/nn2/deal/public',
+        'user'=>'http://124.166.246.120:8000/nn2/user/public',
+        'admin'=>'http://124.166.246.120:8000/nn2/admin/public',
     ),
     'rootDir'=>array(//子系统根目录物理路径
-        'user'=>'D:\king\GGIItt\wamp2\www\nn2\user\public',
-        'admin'=>'D:\king\GGIItt\wamp2\www\nn2\admin\public',
-        'deal'=>'D:\king\GGIItt\wamp2\www\nn2\deal\public'
+        'user'=>'D:\wamp\www\nn2\user\public',
+        'admin'=>'D:\wamp\www\nn2\admin\public',
+        'deal'=>'D:\wamp\www\nn2\deal\public'
     ),
      'sms'=>array(
         'platform'=>'jianzhou',
