16747dc461ef0b155bf1e3789f233fde3e1a198c
wangzhande <15313086535@163.com>
2016-08-25 13:43:00 +0800

    修改bug

--- libs/nainai/system/help.php
+++ libs/nainai/system/help.php
@@ -47,4 +47,34 @@ class help
         $memcache->set('helpList'.$this->helpCatLimit.$this->helpLimit,serialize($helpList));
         return $helpList;
     }
+    public function getAllHelpList(){
+        $memcache=new \Library\cache\driver\Memcache();
+        $helpList=$memcache->get('allHelpList'.$this->helpCatLimit.$this->helpLimit);
+        if($helpList){
+            return unserialize($helpList);
+        }
+        $helpCatObj=new Query('help_category');
+        $helpCatObj->where="status=:status";
+        $helpCatObj->bind=['status'=>1];
+        if($this->helpCatLimit!='') {
+            $helpCatObj->limit = $this->helpCatLimit;
+        }
+        $helpCatObj->order=' sort asc';
+        $helpCatList=$helpCatObj->find();
+        $helpObj=new Query('help');
+        if($this->helpLimit!="") {
+            $helpObj->limit = $this->helpLimit;
+        }
+        $helpObj->order=' sort asc';
+        $helpList=array();
+        foreach($helpCatList as $k=>$v){
+            $helpObj->where='cat_id=:cat_id';
+            $helpObj->bind=['cat_id'=>$v['id']];
+            $helpList[$k]['cat_id']=$v['id'];
+            $helpList[$k]['name']=$v['name'];
+            $helpList[$k]['data']=$helpObj->find();
+        }
+        $memcache->set('allHelpList'.$this->helpCatLimit.$this->helpLimit,serialize($helpList));
+        return $helpList;
+    }
 }
