eef85ff2e0821b13cd274553250d9ccd5be3bc87
zyn1 <739619279@qq.com>
2016-08-26 10:34:35 +0800

    Revert "修改"
    This reverts commit 50942cf9a28f5ae9fa06474511bde384f148b45e.

--- libs/nainai/offer/product.php
+++ libs/nainai/offer/product.php
@@ -265,13 +265,13 @@ class product  {
         if($topCate){
             return unserialize($topCate);
         }
-       // $where  = array();
+        $where  = array('status' => 1, 'is_del' => 0,'pid'=>0);
         if($num)
             $this->_productObj->table('product_category')->limit($num);
         else{
             $this->_productObj->table('product_category');
         }
-        $category = $this->_productObj->fields('id,pid, name, unit, childname, attrs')->order('sort ASC,id DESC')->where('status=1 AND is_del=0 AND pid=0')->select();
+        $category = $this->_productObj->fields('id,pid, name, unit, childname, attrs')->order('sort ASC,id DESC')->where($where)->select();
         $memcache->set('topCate',serialize($category));
         return $category;
     }
