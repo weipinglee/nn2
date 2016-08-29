4f1cfeef951ab9254423c4035e2d4af12c3cec2d
weipinglee <weiping.lee@163.com>
2016-08-29 17:42:58 +0800

    cuow

--- deal/public/index.php
+++ deal/public/index.php
@@ -5,12 +5,6 @@ date_default_timezone_set('Asia/Shanghai');
 
 define('APPLICATION_PATH', dirname(__DIR__));
 
-if(\Library\tool::getConfig('error')){
-    error_reporting(E_ALL);
-}
-else{
-    error_reporting(0);
-}
 
 header("Content-Type:text/html;charset=utf-8");
 
--- user/public/index.php
+++ user/public/index.php
@@ -4,12 +4,6 @@ date_default_timezone_set('Asia/Shanghai');
 
 header("Content-Type:text/html;charset=utf-8");
 
-if(\Library\tool::getConfig('error')){
-    error_reporting(E_ALL);
-}
-else{
-    error_reporting(0);
-}
 define('APPLICATION_PATH', dirname(__DIR__));
 $application = new \Yaf\Application( APPLICATION_PATH . "/conf/application.ini");
 
