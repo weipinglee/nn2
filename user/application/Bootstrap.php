0ea8822ba5cc7e9d63bd7f1d1d4a9b8e0076c04e
weipinglee <weiping.lee@163.com>
2016-09-01 21:18:20 +0800

    domain

--- user/application/Bootstrap.php
+++ user/application/Bootstrap.php
@@ -21,6 +21,7 @@ class Bootstrap extends \Yaf\Bootstrap_Abstract{
 		//把配置保存起来
 		$this->config = Yaf\Application::app()->getConfig();
 		\Yaf\Registry::set('config', $this->config);
+		ini_set("session.cookie_domain",'nainaiwang.com');
 		session_start();
 		define('REQUEST_METHOD', strtoupper($dispatcher->getRequest()->getMethod()));
 		define('IS_GET',        REQUEST_METHOD =='GET' ? true : false);
