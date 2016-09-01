4ed6459adfc25273f67a6adcb993e4cef0af3a6d
weiping <weiping.lee@163.com>
2016-09-01 17:02:33 +0800

    jjk

--- user/application/controllers/Login.php
+++ user/application/controllers/Login.php
@@ -28,7 +28,7 @@ class LoginController extends \Yaf\Controller_Abstract {
      * 对于如下的例子, 当访问http://yourhost/yar-demo/index/index/index/name/root 的时候, 你就会发现不同
      */
 	public function indexAction() {
-
+		$this->redirect(\Library\url::createUrl('/Login/login@user'));
 	}
 
 	public function captchaAction(){
