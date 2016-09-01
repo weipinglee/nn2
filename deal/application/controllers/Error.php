32416c2513b785176a654afd1e072fa1d7e6bc6c
weiping <weiping.lee@163.com>
2016-09-01 15:31:22 +0800

    404‘

--- deal/application/controllers/Error.php
+++ deal/application/controllers/Error.php
@@ -9,8 +9,18 @@ class ErrorController extends Yaf\Controller_Abstract {
 
 	//从2.1开始, errorAction支持直接通过参数获取异常
 	public function errorAction($exception) {
-		//1. assign to view engine
-		$this->getView()->assign("exception", $exception);
-		//5. render by Yaf 
+
+		switch($exception->getCode()) {
+			case 513:
+			case 514:
+			case 515:
+			case 516:
+			case 517:
+			case 518:
+				header( "HTTP/1.1 404 Not Found" );
+				header( "Status: 404 Not Found" );
+				break;
+		}
+
 	}
 }
