2e68a5d65f98e7abd6203f0eda7047b0d575f6a9
weipinglee <weiping.lee@163.com>
2016-10-12 17:44:37 +0800

    1.0daoru

--- user/application/models/User.php
+++ user/application/models/User.php
@@ -331,7 +331,7 @@ class UserModel{
 	 */
 	public function checkUser($userAcc,$password){
 
-		$where = 'username=:username AND password in( :password,:password1) OR mobile=:mobile AND password = in( :password,:password1) AND status=:status';
+		$where = '(username=:username AND (password = :password OR password = :password1) OR mobile=:mobile AND (password = :password OR password = :password1)) AND status=:status';
 		return self::$userObj->fields('id,username,mobile,password,type')->where($where)->bind(array('username'=>$userAcc,'password'=>sha1($password),'password1'=>base64_encode(md5($password,16)),'mobile'=>$userAcc, 'status' => \nainai\user\User::NOMAL))->getObj();
 
 
