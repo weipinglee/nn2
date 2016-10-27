3cdd0d12ef436ea130d5dec46167d35eef5f7e84
panduo <448761421@qq.com>
2016-10-27 10:37:39 +0800

    1027

--- user/views/pc/js/passwordReset.js
+++ user/views/pc/js/passwordReset.js
@@ -212,7 +212,7 @@ $(function () {
                     $('#yzmBtn').attr('disabled',false);
                 }
 
-                $('#image').click();
+                // $('#image').click();
             }
         });
     });
