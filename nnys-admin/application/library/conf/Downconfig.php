36677d832b232c45c5edef4071f2128e80d8c81f
weipinglee <weiping.lee@163.com>
2018-05-02 14:39:19 +0800

    4-28bug修改

--- nnys-admin/application/library/conf/Downconfig.php
+++ nnys-admin/application/library/conf/Downconfig.php
@@ -10,22 +10,22 @@ class Downconfig
           $config = array();
           switch ($type) {
             case 'Acc':
-                $config = array('user_no'=>会员号,'username' => '用户名', 'mobile' => '手机号', 'amount' => '总金额', 'fund' => '可用资金', 'freeze' => '冻结资金');
+                $config = array('user_no'=>'会员号','username' => '用户名', 'mobile' => '手机号', 'amount' => '总金额', 'fund' => '可用资金', 'freeze' => '冻结资金');
                 break;
             case 'Credit':
                 $config = array('username' => '用户名', 'mobile' => '手机号', 'credit' => '信誉保证金');
                 break;
            case 'user_bank':
-                $config = array('user_no'=>会员号,'username' => '用户名', 'true_name' => '姓名', 'bank_name' => '开户银行', 'card_type' => '银行卡类型', 'identify_no' => '身份证号', 'status_text' => '状态');
+                $config = array('user_no'=>'会员号','username' => '用户名', 'true_name' => '姓名', 'bank_name' => '开户银行', 'card_type' => '银行卡类型', 'identify_no' => '身份证号', 'status_text' => '状态');
                 break;
             case 'withdraw_request':
-                $config = array('user_no'=>会员号,'username' => '用户名', 'mobile' => '手机号', 'request_no' => '订单号', 'amount' => '金额', 'status_text' => '状态', 'create_time' => '时间');
+                $config = array('user_no'=>'会员号','username' => '用户名', 'mobile' => '手机号', 'request_no' => '订单号', 'amount' => '金额', 'status_text' => '状态', 'create_time' => '时间');
                 break;
             case 'recharge_orderline':
-                $config = array('user_no'=>会员号,'username' => '用户名', 'order_no' => '订单号', 'amount' => '金额','pay_type'=>'充值方式', 'status_text' => '状态', 'create_time' => '时间');
+                $config = array('user_no'=>'会员号','username' => '用户名', 'order_no' => '订单号', 'amount' => '金额','pay_type'=>'充值方式', 'status_text' => '状态', 'create_time' => '时间');
                 break;
               case 'recharge_orderoff':
-                $config = array('user_no'=>会员号,'username' => '用户名', 'order_no' => '订单号', 'amount' => '金额', 'status_text' => '状态', 'create_time' => '时间');
+                $config = array('user_no'=>'会员号','username' => '用户名', 'order_no' => '订单号', 'amount' => '金额', 'status_text' => '状态', 'create_time' => '时间');
                 break;
               case 'dealer':
                 $config = array('username' => '登录账号', 'type_text' => '会员类型', 'mobile' => '手机号', 'status_text' => '认证状态', 'apply_time' => '申请时间');
