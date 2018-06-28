db61dd4d34836edcb7dc1ce3ab8c0f1185b50af0
weipinglee <weiping.lee@163.com>
2018-06-28 09:28:05 +0800

    后台报盘审核对竞价报盘生成event

--- nnys-admin/application/models/OfferManage.php
+++ nnys-admin/application/models/OfferManage.php
@@ -169,7 +169,7 @@ class OfferManageModel extends \nainai\offer\product{
 		if(!($id = intval($id))) return tool::getSuccInfo(0,'参数错误');
 		$status = isset($status) ? intval($status) : 1;
 
-		$offerData = $this->offer->where(array('id'=>$id))->fields('user_id,acc_type,mode,sub_mode,offer_fee,status,product_id,type')->getObj();
+		$offerData = $this->offer->where(array('id'=>$id))->fields('user_id,old_offer,acc_type,mode,sub_mode,offer_fee,status,product_id,type')->getObj();
 
 		if($offerData['status']!=self::OFFER_APPLY){
 			return tool::getSuccInfo(0,'该报盘已审核');
@@ -230,6 +230,11 @@ class OfferManageModel extends \nainai\offer\product{
 			}
 
 			$res = $this->offer->commit();
+			//事务成功且设置状态为通过，报盘是非转的竞价，生成mysql event
+			if($res===true && $status==self::OFFER_OK && $offerData['sub_mode']==1 &&$offerData['old_offer']==0){
+			    $jingjiaObj = new \nainai\offer\jingjiaOffer();
+			    $jingjiaObj->createXinEvent($id);
+            }
 		} catch (PDOException $e) {
 			$this->offer->rollBack();
 			$res = $e->getMessage();
